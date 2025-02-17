b97b96dc021bff00e86799e49c15ffa9c990c034
sz-99 <zhengsusan3@gmail.com>
2025-02-17 16:32:10 +0000

    FEAT: add model class for User

---
+++ InterArt/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace InterArt.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public int Level { get; set; } = 1;
+        public int XP { get; set; } = 0;
+
+    }
+}
