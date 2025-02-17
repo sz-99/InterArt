2147ad8feec41d9760a2c44569d13e7827a012fa
sz-99 <zhengsusan3@gmail.com>
2025-02-17 16:45:57 +0000

    FEAT: Add model class for artwork

---
+++ InterArt/Models/Artwork.cs
@@ -0,0 +1,16 @@
+﻿namespace InterArt.Models
+{
+    public class Artwork
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } =string.Empty;
+        public string Artist { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string PlaceOfOrigin { get; set; } = string.Empty;
+        public string Medium {  get; set; } = string.Empty;
+        public string ImageId {  get; set; } = string.Empty;
+        public 
+
+    }
+}
