9d6647708dcffc9df6939213518da9a0ce03152d
yinglexing <xingying0824@icloud.com>
2025-02-24 15:51:57 +0000

    renamed folder

--- InterArt/Models/DTOs/UserLoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InterArt_Backend.Models.DTOs
-{
-    public class UserLoginDTO
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-
-    }
-}
