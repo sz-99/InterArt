db3632948982749e3e612b63adc32a78f0f49bbb
sz-99 <zhengsusan3@gmail.com>
2025-03-04 17:52:33 +0000

    FEAT: Add CORS policy to make calls on frontend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -11,7 +11,13 @@ using Newtonsoft.Json.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy => policy.WithOrigins("http://localhost:5174") // React frontend URL
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -47,6 +53,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 
 }
+app.UseCors("AllowReactApp"); 
 
 app.UseHttpsRedirection();
 
